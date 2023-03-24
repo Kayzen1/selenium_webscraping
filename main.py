dd6d45f57e545aa3bd721537347f7a38ce030d5f
Zeng Yuting <kay@Zengs-MacBook-Pro.local>
2023-03-24 18:22:39 -0500

    add more columns

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-starttime = time()
+starttime = time.time()
 PATH = "/Users/kay/PycharmProjects/selenium_health/chromedriver"
 service = Service(PATH)
 driver = WebDriver(service=service)
-driver.implicitly_wait(10)
+driver.implicitly_wait(1)
 
 url = "https://www.healthgrades.com/usearch?what=Family%20Medicine&distances=National"
 driver.get(url)
@@ -24,8 +24,8 @@ press = driver.find_elements(by=By.CLASS_NAME, value='pRyBq')
 link_list = []
 
 page_count = 0
-condition = True
-while condition:
+
+while True:
     doctor_links = driver.find_elements(by=By.CLASS_NAME, value='card-name')
     # find doctors' personal webpage links
     for doctor_link in doctor_links:
@@ -37,9 +37,10 @@ while condition:
     page_count = page_count+1
     # print(pagecount)
     if page_count == 1:
-        condition = False
+        break
     # click to next page
     driver.execute_script("arguments[0].click();", nextpage[-1])
+    # WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'card-name')))
 
 
 all_details = []
@@ -48,24 +49,46 @@ for i in tqdm(link_list):
     driver.get(i)
     d_name = driver.find_element(by=By.TAG_NAME, value='h1').text
     d_speciality = driver.find_element(By.CSS_SELECTOR, 'span[data-qa-target = "ProviderDisplaySpeciality"]').text
+    total_score = driver.find_element(By.CSS_SELECTOR, 'div.overall-rating p').text
+    total_survey_count = driver.find_element(By.CSS_SELECTOR, 'div.overall-rating div p').text
+    five_star = driver.find_element(By.CSS_SELECTOR, 'table.breakdown-table tr td').text
+    five_star_percentage = driver.find_element(By.CSS_SELECTOR, 'table.breakdown-table tr td.breakdown-table__bar span').text
+    four_star = driver.find_element(By.CSS_SELECTOR, 'table.breakdown-table tbody tr:nth-child(2) td').text
+    four_star_percentage = driver.find_element(By.CSS_SELECTOR, 'table.breakdown-table tbody tr:nth-child(2) td.breakdown-table__bar span').text
+    three_star = driver.find_element(By.CSS_SELECTOR,
+                                     'table.breakdown-table tbody tr:nth-child(3) td').text
+    three_star_percentage = driver.find_element(By.CSS_SELECTOR,
+                                               'table.breakdown-table tbody tr:nth-child(3) td.breakdown-table__bar span').text
 
-
+    two_star = driver.find_element(By.CSS_SELECTOR,
+                                               'table.breakdown-table tbody tr:nth-child(4) td').text
+    two_star_percentage = driver.find_element(By.CSS_SELECTOR,
+                                  'table.breakdown-table tbody tr:nth-child(4) td.breakdown-table__bar span').text
+    one_star = driver.find_element(By.CSS_SELECTOR,
+                                  'table.breakdown-table tbody tr:nth-child(5) td').text
+    one_star_percentage = driver.find_element(By.CSS_SELECTOR,
+                                  'table.breakdown-table tbody tr:nth-child(5) td.breakdown-table__bar span').text
     tempJ = {
         'd_name': d_name,
         'd_speciality': d_speciality,
         # 'd_age': d_age,
         # 'd_gender': d_gender,
-        # 'total_score': total_score,
-        # 'total_num_rate': total_num_rate,
-        # '5star': 5star,
-        # '4star': 4star,
-        # '3star': 3star,
-        # '2star': 2star,
-        # '1star': 1star,
+        'total_score': total_score,
+        'total_num_rate': total_survey_count,
+        '5star': five_star,
+        '5_percentage': five_star_percentage,
+        '4star': four_star,
+        '4_percentage': four_star_percentage,
+        '3star': three_star,
+        '3_percentage': three_star_percentage,
+        '2star': two_star,
+        '2_percentage':two_star_percentage,
+        '1star': one_star,
+        '1_percentage':one_star_percentage,
     }
     all_details.append(tempJ)
     driver.close()
 df = pd.DataFrame(all_details)
 df.to_csv('doctor.csv')
-endtime = time()
-print(endtime-starttime)
+endtime = time.time()
+print((endtime-starttime))
