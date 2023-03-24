dd3af8363ccd2360304783983c8f1c703599fe29
Zeng Yuting <kay@Zengs-MacBook-Pro.local>
2023-03-24 17:21:00 -0500

    find out speciality css path and add time counting

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import pandas as pd
 import numpy as np
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.chrome.service import Service
-
+import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-# with open('doctor.csv', 'w') as f:
-#     f.write()
+
+
+starttime = time()
 PATH = "/Users/kay/PycharmProjects/selenium_health/chromedriver"
 service = Service(PATH)
 driver = WebDriver(service=service)
@@ -29,9 +30,9 @@ while condition:
     # find doctors' personal webpage links
     for doctor_link in doctor_links:
         link = doctor_link.find_element(by=By.TAG_NAME, value='a')
+        # store each link to a empty list
         link_list.append(link.get_attribute('href'))
-        # d_speciality = driver.find_element(By.CSS_SELECTOR, '.provider-info__specialty span').text
-        #store each link to a empty list
+
     nextpage = driver.find_elements(By.XPATH, '//*[@id="search-results"]/div/div[2]/div[2]/div[1]/nav/ul/li/a')
     page_count = page_count+1
     # print(pagecount)
@@ -46,12 +47,12 @@ for i in tqdm(link_list):
     driver = WebDriver(service=service)
     driver.get(i)
     d_name = driver.find_element(by=By.TAG_NAME, value='h1').text
-    # d_speciality = driver.find_elements(By.CSS_SELECTOR, '.summary-header-row-specialty span')
+    d_speciality = driver.find_element(By.CSS_SELECTOR, 'span[data-qa-target = "ProviderDisplaySpeciality"]').text
 
 
     tempJ = {
         'd_name': d_name,
-        # 'd_speciality': d_speciality,
+        'd_speciality': d_speciality,
         # 'd_age': d_age,
         # 'd_gender': d_gender,
         # 'total_score': total_score,
@@ -66,3 +67,5 @@ for i in tqdm(link_list):
     driver.close()
 df = pd.DataFrame(all_details)
 df.to_csv('doctor.csv')
+endtime = time()
+print(endtime-starttime)
