a9ab25db040b4cb8df66806f961ba138878b1ed9
Zeng Yuting <kay@Zengs-MacBook-Pro.local>
2023-04-03 21:07:32 -0500

    add comments

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.common.by import By
-from tqdm import tqdm
+import csv
+import time
+from concurrent.futures import ThreadPoolExecutor
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
-import time
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-import pandas as pd
-from concurrent.futures import ThreadPoolExecutor
-import concurrent.futures
-import csv
+from selenium.webdriver.support.ui import WebDriverWait
+from tqdm import tqdm
 
+'''
+Check whether element can be found or not
+'''
 
 def iselement(browser, cssselector):
     try:
@@ -20,8 +21,9 @@ def iselement(browser, cssselector):
         return False
 
 
-starttime = time.time()
-
+'''
+Set up driver
+'''
 
 def get_driver():
     PATH = "/Users/kay/PycharmProjects/selenium_health/selenium_webscraping/chromedriver"
@@ -31,46 +33,70 @@ def get_driver():
     return driver
 
 
+'''
+If target page number is 99, then this function will append all links from page 1 to page 99 to a list
+'''
+
 def get_doc_linklist(url):
     driver = get_driver()
     driver.get(url)
-
     link_list = []
     page_count = 0
 
     while True:
         doctor_links = driver.find_elements(by=By.CLASS_NAME, value='card-name')
-        # find doctors' personal webpage links
+        # Find doctors' personal webpage links
         for doctor_link in doctor_links:
             link = doctor_link.find_element(by=By.TAG_NAME, value='a')
-            # store each link to a empty list
+            # Store each link to an empty list
             link_list.append(link.get_attribute('href'))
 
         nextpage = driver.find_elements(By.XPATH, '//*[@id="search-results"]/div/div[2]/div[2]/div[1]/nav/ul/li/a')
         page_count = page_count + 1
-        # print(pagecount)
+
         if page_count == 10:
             break
-        # click to next page
+        # Click to next page
         driver.execute_script("arguments[0].click();", nextpage[-1])
     driver.close()
     return link_list
 
 
-# press = driver.find_elements(by=By.CLASS_NAME, value='pRyBq')
+'''
+Get all doctor's information. Including name, speciality, tags and comments
+'''
 
 def get_doctor_details(link):
     driver = get_driver()
     driver.get(link)
     driver.implicitly_wait(1)
     d_name = driver.find_element(By.CSS_SELECTOR, 'div h1').text
-    print("1:", d_name)
 
+    # Some doctor might not have speciality
     if (iselement(driver, 'span[data-qa-target = "ProviderDisplaySpeciality"]')):
         d_speciality = driver.find_element(By.CSS_SELECTOR, 'span[data-qa-target = "ProviderDisplaySpeciality"]').text
     else:
         d_speciality = 'NA'
 
+    try:
+        # Close ad
+        ad_button = WebDriverWait(driver, 2).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.bottom-ad-close')))
+        ad_button.click()
+    except:
+        pass
+
+    try:
+        # Close cookie. Cookie will cover other button
+        cookie_button = WebDriverWait(driver, 4).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, 'div#onetrust-close-btn-container button')))
+        cookie_button.click()
+    except:
+        pass
+
+    driver.execute_script("window.scrollBy(0, 1000);")
+
+    # Check the existence of tag section
     if (iselement(driver, 'div.ratings-wrapper')):
         total_score = driver.find_element(By.CSS_SELECTOR, 'div.overall-rating p').text
         total_survey_count = driver.find_element(By.CSS_SELECTOR, 'div.overall-rating div p').text
@@ -94,6 +120,7 @@ def get_doctor_details(link):
         one_star_percentage = driver.find_element(By.CSS_SELECTOR,
                                                   'table.breakdown-table tbody tr:nth-child(5) td.breakdown-table__bar span').text
     else:
+        # Make sure if there is no tag section, data will be entered into the csv
         total_score = 'NA'
         total_survey_count = 'NA'
         five_star = 'NA'
@@ -109,33 +136,16 @@ def get_doctor_details(link):
 
     driver.execute_script("window.scrollBy(0, 1000);")
 
-    try:
-        cookie_button = WebDriverWait(driver, 2).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, 'div#onetrust-close-btn-container button')))
-        # cookie_button = driver.find_element(By.CSS_SELECTOR, 'div#onetrust-close-btn-container button')
-        cookie_button.click()
-    except:
-        pass
-
-    try:
-        ad_button = WebDriverWait(driver, 2).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.bottom-ad-close')))
-        # ad_button = driver.find_element(By.CSS_SELECTOR, 'a.bottom-ad-close')
-        ad_button.click()
-    except:
-        pass
-
     positive_tags = []
     negative_tags = []
     if iselement(driver, 'div.review-tagging-summary'):
-        # print("2:", d_name)
         try:
-            # click on "more" buttons
+            # Click on "more" buttons
             more_button = WebDriverWait(driver, 2).until(
                 EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.link--secondary')))
             driver.execute_script("arguments[0].click();", more_button)
         except:
-            # print("there is no more button for the tag")
+            # There is no more button for the tag
             pass
 
         try:
@@ -145,7 +155,7 @@ def get_doctor_details(link):
             for tag in po_tags:
                 positive_tags.append(tag.text)
         except:
-            # print("there is no po_tags")
+            # There is no positive tags
             pass
 
         try:
@@ -155,14 +165,13 @@ def get_doctor_details(link):
             for tag in ne_tags:
                 negative_tags.append(tag.text)
         except:
-            # print("there is no ne_tags")
+            # print("there is no negative tags")
             pass
 
     num_reviews_before = len(driver.find_elements(By.CSS_SELECTOR, 'div.c-single-comment__comment'))
     while True:
-        # print("3:", d_name)
         try:
-            # click on "more reviews"button
+            # click on "more reviews" button
             more_reviews_button = WebDriverWait(driver, 2).until(
                 EC.element_to_be_clickable((By.CSS_SELECTOR, 'a.c-comment-list__show-more')))
 
@@ -170,44 +179,39 @@ def get_doctor_details(link):
             time.sleep(2)
             num_reviews_after = len(driver.find_elements(By.CSS_SELECTOR, 'div.c-single-comment__comment'))
             print(num_reviews_after, num_reviews_before)
-                # print(d_name)
 
             if num_reviews_after == num_reviews_before:
-                    # print("No new reviews loaded. End of reviews.")
+                # No new reviews loaded. End of reviews.
                 break
             num_reviews_before = num_reviews_after
 
-                # check if "more details" button is present
+            # check if "more details" button is present
             try:
                 more_comment_detail = WebDriverWait(driver, 1).until(
                     EC.element_to_be_clickable(
                         (By.CSS_SELECTOR, 'div.c-single-comment__more-details button.link--secondary')))
                 driver.execute_script("arguments[0].click();", more_comment_detail)
             except:
-                # print("there is no 'more detail button' for comment")
+                # there is no 'more detail button' for comment
                 pass
-
         except:
-            # print("4:", d_name)
-            print("No 'more' button found on the page.")
+            # No 'more' button found on the page.
             break
+
     if iselement(driver, 'div.c-single-comment'):
         try:
             comments = WebDriverWait(driver, 2).until(
-                    EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div[data-qa-target="user-comment"]')))
+                EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div[data-qa-target="user-comment"]')))
             for comment in comments:
                 comment_text = comment.text
                 rows.append(
-                        [d_name, d_speciality, total_score, total_survey_count, five_star, five_star_percentage, four_star,
-                        four_star_percentage,
-                        three_star, three_star_percentage, two_star, two_star_percentage, one_star, one_star_percentage,
-                        positive_tags, negative_tags,
-                        comment_text])
-
+                    [d_name, d_speciality, total_score, total_survey_count, five_star, five_star_percentage, four_star,
+                     four_star_percentage,
+                     three_star, three_star_percentage, two_star, two_star_percentage, one_star, one_star_percentage,
+                     positive_tags, negative_tags,
+                     comment_text])
         except:
-                # print("there is no comment for the doctor")
-
-                pass
+            pass
     else:
         comment_text = 'NA'
         rows.append(
@@ -219,27 +223,22 @@ def get_doctor_details(link):
     driver.quit()
     return rows
 
+
+starttime = time.time()
 url = "https://www.healthgrades.com/usearch?what=Family%20Medicine&distances=National"
 link_list = get_doc_linklist(url)
-# print("link:", len(link_list))
-# all_details = []
+
 rows = []
-# more_comment_detail = None
-# for i in tqdm(link_list):
-#     all_details.append(get_doctor_details(i))
 
-
+# Multithreading. Click on each link and process get_doctor_details(each_link)
 with ThreadPoolExecutor(max_workers=10) as executor:
     for _ in tqdm(executor.map(get_doctor_details, link_list), total=len(link_list)):
         pass
 
-# print("rows length:", len(rows))
-# df = pd.DataFrame(all_details)
-# df.to_csv('doctor.csv')
-
-header = ['d_name', 'd_speciality', 'total_score', 'total_survey_count', 'five_star', 'five_star_percentage', 'four_star',
+header = ['d_name', 'd_speciality', 'total_score', 'total_survey_count', 'five_star', 'five_star_percentage',
+          'four_star',
           'four_star_percentage', 'three_star', 'three_star_percentage', 'two_star', 'two_star_percentage', 'one_star',
-          'one_star_percentage','positive_tags', 'negative_tags', 'comment_text']
+          'one_star_percentage', 'positive_tags', 'negative_tags', 'comment_text']
 
 with open('doctor.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
